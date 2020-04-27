0e5471166f03a3dc59899654e7ff40d764e9942e
matt_kuras <mattkuras@gmail.com>
2020-04-27 16:01:18 -0400

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-User.create(email: "mattkuras@gmail.com", username: "matt", password: "password")
-User.create(email: "mikejorand@gmail.com", username: "mike", password: "password")
+matt = User.create(email: "mattkuras@gmail.com", username: "matt", password: "password")
+mike = User.create(email: "mikejorand@gmail.com", username: "mike", password: "password")
 
-matt.hikes.create(location: "Brick,")
+matt.hikes.create(name: "Devil's Pass", rating: 8.9, difficulty: "medium")
+mike.hikes.create(name: "Long Range Traverse", rating: 10.0, difficulty: "hard")
