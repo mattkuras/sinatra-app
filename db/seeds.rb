5012136fb97e002d4d19fcad6c9fbff612168d66
matt_kuras <mattkuras@gmail.com>
2020-04-27 15:49:50 -0400

    created seeds.rb file

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create(email: "mattkuras@gmail.com", username: "matt", password: "password")
+User.create(email: "mikejorand@gmail.com", username: "mike", password: "password")
+
+matt.hikes.create(location: "Brick,")
