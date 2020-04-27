d7c424810fa7fb407b301a4b0973124269cc6156
matt_kuras <mattkuras@gmail.com>
2020-04-27 17:33:49 -0400

    fill out post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,19 @@ class UsersController < ApplicationController
     end
 
     # receiver login form, find user, add user id to session
-    post '/login' do 
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+     #   if @user.authenticate(params[:username])
+     if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            redirect to "/users/#{@user.id}"
+        else
+            redirect to '/login'
+        end
+    end
 
+    get '/users/:id' do
+        "show route"
     end
 
     #render signup page 
