da200e3798788be0a52889fa7ceffe1940cb63e2
matt_kuras <mattkuras@gmail.com>
2020-04-29 16:45:30 -0400

    fix new comment methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,6 @@ class CommentsController < ApplicationController
         else
             @comment = Comment.create(params)
             current_user.comments << @comment 
-            binding.pry
             redirect to "/hikes"
         end
     end
